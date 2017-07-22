8a47fc7c749230425c5baf32b5b1da71149c99e6
李亮 <admin@wlitsoft.com>
2017-07-22 10:44:59 +0800

    自动共建 补充nuget 包依赖。。。

--- src/Common.Logger.Log4Net/Globals/VersionInfo.cs
+++ src/Common.Logger.Log4Net/Globals/VersionInfo.cs
@@ -18,5 +18,5 @@ using System.Reflection;
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.2016.1217")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.2017.0722")]
